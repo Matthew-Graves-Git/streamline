e5599f70b1239bdf76df81c83e67e0e0439c1c68
Matthew Graves <matthew.graves.cs@gmail.com>
2023-08-23 00:45:43 -0400

    initial text/voice app

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import {AiOutlinePlus} from 'react-icons/ai';
+
+
+const Navbar = () => {
+const baseIcons = [
+    {
+        name:'DM',
+        description:'Direct Messages',
+        icon:<><AiOutlinePlus/></>
+    }
+]
+    return ( 
+        <nav>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
